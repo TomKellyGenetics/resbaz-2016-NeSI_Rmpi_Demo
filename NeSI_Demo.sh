061160d9c7ce28484357795c46d764691179b57d
TomKellyGenetics <tomkellygenetics@gmail.com>
2016-02-01 16:10:01 +1300

    remove test from shell script

--- NeSI_Demo.sh
+++ NeSI_Demo.sh
@@ -5,18 +5,11 @@ ssh pan
 
 cd /projects/uoo00010
 mkdir NeSI_Demo
+pwd
 exit
 
 scp *R pan:/projects/uoo00010/NeSI_Demo
-scp -c arcfour * pan:/projects/uoo00010/NeSI_Demo
-
-ssh pan
-cd /projects/uoo00010/NeSI_Demo
-sbatch NeSI_Demo_parallel_test.sl
-squeue -u simon.kelly
-
-
-scp *R pan:/projects/uoo00010/NeSI_Demo
+scp -c arcfour *RData pan:/projects/uoo00010/NeSI_Demo
 scp *sl pan:/projects/uoo00010/NeSI_Demo
 
 ssh pan
